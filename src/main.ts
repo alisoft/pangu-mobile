8703057fd8edb3440b63f8ec7fccd49ad190f22c
see <upcwangying@126.com>
2022-07-24 16:30:26 +0800

    refactor: `mobile`

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
-import { createApp } from "vue";
-import { createWebHistory } from "vue-router";
-import Vant from "vant";
+import { createSSRApp } from "vue";
+import { createPinia } from "pinia";
+
 import App from "./App.vue";
-import createRouter from "./router";
+import { createRouter } from "./router";
 
-import "vant/lib/index.css";
+import "./assets/main.css";
 
-const app = createApp(App);
-const router = createRouter(createWebHistory("/mobile/"));
-
-app.use(Vant);
-app.use(router);
-
-router.isReady().then(() => {
-  app.mount("#app");
-});
+export function createApp() {
+  const app = createSSRApp(App);
+  const router = createRouter();
+  const store = createPinia();
+  app.use(store);
+  app.use(router);
+  return { app, router };
+}
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,31 @@
-import { createRouter, RouterHistory } from "vue-router";
+import {
+  createMemoryHistory,
+  createRouter as _createRouter,
+  createWebHistory,
+} from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const routes = [
-  { path: "/", redirect: "/demo" },
-  { path: "/about", component: () => import("../views/About.vue") },
-  { path: "/demo", component: () => import("../views/Demo.vue") },
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import("../views/AboutView.vue"),
+  },
 ];
 
-export default function (history: RouterHistory) {
-  return createRouter({
-    history,
+export function createRouter() {
+  return _createRouter({
+    // use appropriate history implementation for server/client
+    // import.meta.env.SSR is injected by Vite.
+    history: import.meta.env.SSR ? createMemoryHistory("/mobile/") : createWebHistory("/mobile/"),
     routes,
   });
 }
